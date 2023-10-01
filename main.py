e88203988c0544aeea7e065135094babc7cff771
0Armaan025 <armaan33000@gmail.com>
2023-10-01 12:59:18 +0530

    doing

--- main.py
+++
@@ -1,7 +0,0 @@
-# Just setting up the enviorment right now. This is a very basic streamlit test
-import streamlit as st
-
-btn = st.button("When I get clicked, there should be new text")
-
-if btn:
-    st.write("See, this is the new text")
